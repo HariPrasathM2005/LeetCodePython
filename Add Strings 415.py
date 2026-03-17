d79be4a15eef7a2eded9eeb612c6d76ba534f666
HariPrasathM2005 <mo.hariprasath@gmail.com>
2026-03-17 16:31:33 +0530

    Implement addStrings function to add two strings

---
+++ Add Strings 415.py
@@ -0,0 +1,9 @@
+def addStrings(num1, num2):
+    n1=int(num1)
+    n2=int(num2)
+    n3=n1+n2
+    n3=str(n3)
+    return n3
+if __name__ == "__main__":
+    val=addStrings("10","12")
+    print(val)
